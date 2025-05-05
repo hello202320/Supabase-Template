f178592d830f6589cb196b3a7546775c441e14b4
druidletsplay2022@gmail.com <vdmartinez17@gmail.com>
2025-05-05 15:27:51 -0400

    [system] added new cors

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:4200") 
+        policy.WithOrigins("http://localhost:4200", "https://v101.netlify.app/") 
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
